11e88dcda2e47d7d0e1bdbfc8b274770365b079d
athulk <athulk@qburst.com>
2021-04-24 15:36:38 +0530

    V2 release

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDxa8-jbWJIvhWxG3KE7x1lSwulbSsTo2c",
-    authDomain: "automated-irrigation-system-0.firebaseapp.com",
-    databaseURL: "https://automated-irrigation-system-0.firebaseio.com",
-    projectId: "automated-irrigation-system-0",
-    storageBucket: "automated-irrigation-system-0.appspot.com",
-    messagingSenderId: "740681487120",
-    appId: "1:740681487120:web:c0961f27f95c3ed4dc4b95",
-    measurementId: "G-87DEMHT6N7"
+    apiKey: "AIzaSyBxEibcQurj-ykiBZ_wtolrdvuB4iDO-Bk",
+    authDomain: "auto-irrigation-system-live.firebaseapp.com",
+    projectId: "auto-irrigation-system-live",
+    storageBucket: "auto-irrigation-system-live.appspot.com",
+    messagingSenderId: "184596316246",
+    appId: "1:184596316246:web:35df19864c2eb3d78e3bf7",
+    measurementId: "G-HV9KZQ7043"
   },
 };
